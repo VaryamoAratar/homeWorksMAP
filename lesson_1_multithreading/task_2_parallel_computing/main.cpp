1cee9cd4996ce16271e8415530239f68a2ce85be
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-06-29 13:25:13 +0400

    task2_fin

--- lesson_1_multithreading/task_2_parallel_computing/main.cpp
+++ lesson_1_multithreading/task_2_parallel_computing/main.cpp
@@ -2,200 +2,127 @@
 #include <chrono>
 #include <thread>
 #include <vector>
-#include <algorithm>
-#include <execution>
+#include <exception>
 
-enum stream_number
-{
+enum stream_number {
 	ONE = 1,
-	TWO = 2,
-	FOUR = 4,
-	EIGHT = 8,
-	SIXTEEN = 16
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN,
+	ELEVEN,
+	TWELVE,
+	THIRTEEN,
+	FOURTEEN,
+	FIFTEEN,
+	SIXTEEN
 };
 
-void v_sum(std::vector<int>& result, std::vector<int>& v1, std::vector<int>& v2, const int begin, const int end)
-{
-
-
-	for (int i = begin; i < end; i++)
-	{
+void v_sum(std::vector<int>& result, std::vector<int>& v1, std::vector<int>& v2, const int begin, const int end) {
+	for (int i = begin; i < end; i++) {
 		result[i] = v1[i] + v2[i];
 	}
 }
 
-float calcTime (int size, int number_of_threads) 
-{
-std::vector<int> v(size);
+float calcTime(int size, int number_of_threads) {
+	std::vector<int> v(size);
 	std::vector<int> v1(v.size(), 1);
 	std::vector<int> v2(v.size(), 1);
 
-const auto start = std::chrono::steady_clock::now();
+	const auto start = std::chrono::steady_clock::now();
 
-if (number_of_threads == stream_number::ONE)
-{
-		std::thread t{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), 0 , v.size()};
-		t.join();
-}
-else if (number_of_threads == stream_number::TWO)
-{
-	int end1 = v.size() / number_of_threads;
-	int begin2 = end1;
-	std::thread t1{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), 0 , end1 };
-	std::thread t2{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin2 , v.size()};
-	t1.join();
-	t2.join();
-}
-else if (number_of_threads == stream_number::FOUR)
-{
-	int end1 = v.size() / number_of_threads;
-	int begin2 = end1;
-	int end2 = end1 * 2;
-	int begin3 = end2;
-	int end3 = end1 * 3;
-	int begin4 = end3;
-	std::thread t1{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), 0 , end1 };
-	std::thread t2{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin2 , end2 };
-	std::thread t3{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin3 , end3 };
-	std::thread t4{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin4 , v.size() };
-	t1.join();
-	t2.join();
-	t3.join();
-	t4.join();
-}
-else if (number_of_threads == stream_number::EIGHT)
-{
-	int end1 = v.size() / number_of_threads;
-	int begin2 = end1;
-	int end2 = end1 * 2;
-	int begin3 = end2;
-	int end3 = end1 * 3;
-	int begin4 = end3;
-	int end4 = end1 * 4;
-	int begin5 = end4;
-	int end5 = end1 * 5;
-	int begin6 = end5;
-	int end6 = end1 * 6;
-	int begin7 = end6;
-	int end7 = end1 * 7;
-	int begin8 = end7;
+	if (number_of_threads == stream_number::ONE) {
+		v_sum(v, v1, v2, 0, v.size());
+	}
+	else {
+		int itr = v.size() / number_of_threads;
+		std::vector<std::thread> threads;
+		for (int i = 1; i <= number_of_threads; i++) {
+			if (i == number_of_threads) {
+				threads.push_back(std::thread(v_sum, std::ref(v), std::ref(v1), std::ref(v2), (itr * (i - 1)), v.size()));
+			}
+			else {
+				threads.push_back(std::thread(v_sum, std::ref(v), std::ref(v1), std::ref(v2), (itr * (i - 1)), (itr * i)));
+			}
+		}
+		for (int i = 0; i < threads.size(); i++) {
+			threads[i].join();
+		}
 
-	std::thread t1{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), 0 , end1 };
-	std::thread t2{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin2 , end2 };
-	std::thread t3{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin3 , end3 };
-	std::thread t4{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin4 , end4 };
-	std::thread t5{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin5 , end5 };
-	std::thread t6{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin6 , end6 };
-	std::thread t7{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin7 , end7 };
-	std::thread t8{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin8 , v.size() };
-	t1.join();
-	t2.join();
-	t3.join();
-	t4.join();
-	t5.join();
-	t6.join();
-	t7.join();
-	t8.join();
-}
-else if (number_of_threads == stream_number::SIXTEEN)
-{
-	int end1 = v.size() / number_of_threads;
-	int begin2 = end1;
-	int end2 = end1 * 2;
-	int begin3 = end2;
-	int end3 = end1 * 3;
-	int begin4 = end3;
-	int end4 = end1 * 4;
-	int begin5 = end4;
-	int end5 = end1 * 5;
-	int begin6 = end5;
-	int end6 = end1 * 6;
-	int begin7 = end6;
-	int end7 = end1 * 7;
-	int begin8 = end7;
-	int end8 = end1 * 8;
-	int begin9 = end1;
-	int end9 = end1 * 9;
-	int begin10 = end9;
-	int end10 = end1 * 10;
-	int begin11 = end10;
-	int end11 = end1 * 11;
-	int begin12 = end11;
-	int end12 = end1 * 12;
-	int begin13 = end12;
-	int end13 = end1 * 13;
-	int begin14 = end13;
-	int end14 = end1 * 14;
-	int begin15 = end14;
-	int end15 = end1 * 15;
-	int begin16 = end15;
-
-	std::thread t1{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), 0 , end1 };
-	std::thread t2{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin2 , end2 };
-	std::thread t3{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin3 , end3 };
-	std::thread t4{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin4 , end4 };
-	std::thread t5{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin5 , end5 };
-	std::thread t6{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin6 , end6 };
-	std::thread t7{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin7 , end7 };
-	std::thread t8{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin8 , end8 };
-	std::thread t9{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin9 , end9 };
-	std::thread t10{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin10 , end10 };
-	std::thread t11{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin11 , end11 };
-	std::thread t12{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin12 , end12 };
-	std::thread t13{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin13 , end13 };
-	std::thread t14{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin14 , end14 };
-	std::thread t15{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin15 , end15 };
-	std::thread t16{ v_sum, std::ref(v), std::ref(v1), std::ref(v2), begin16 , v.size() };
-
-	t1.join();
-	t2.join();
-	t3.join();
-	t4.join();
-	t5.join();
-	t6.join();
-	t7.join();
-	t8.join();
-	t9.join();
-	t10.join();
-	t11.join();
-	t12.join();
-	t13.join();
-	t14.join();
-	t15.join();
-	t16.join();
-}
-
+	}
 	const auto end = std::chrono::steady_clock::now();
 	const auto calcTime = std::chrono::duration<float>(end - start).count();
 	return calcTime;
 }
-void print_result(int size_1, int size_2, int size_3, int size_4, int number_of_threads)
-{
-		std::cout << number_of_threads << " потоков\t" << calcTime(size_1, number_of_threads) << '\t' << calcTime(size_2, number_of_threads) << '\t'
-			<< calcTime(size_3, number_of_threads) << '\t' << calcTime(size_4, number_of_threads) << '\t' << std::endl;
+
+void print_result(const std::vector<int>& sizes, int number_of_threads) {
+	switch (number_of_threads) {
+	case stream_number::ONE: std::cout << number_of_threads << " поток  \t";
+		break;
+	case stream_number::TWO: std::cout << number_of_threads << " потока \t";
+		break;
+	case stream_number::THREE: std::cout << number_of_threads << " потока \t";
+		break;
+	case stream_number::FOUR: std::cout << number_of_threads << " потока \t";
+		break;
+	default: std::cout << number_of_threads << " потоков\t";
+		break;
+	}
+
+	for (const auto i : sizes) {
+		std::cout << calcTime(i, number_of_threads) << '\t';
+	}
+	std::cout << std::endl;
 }
 
-int main(int argc, char* agrv[])
-{
+void print_header(int size) {
+	static bool flag{ false };
+	if (!flag) {
+		auto id2 = std::thread::hardware_concurrency();
+		std::cout << "Количество аппаратных ядер - " << '\t' << id2 << std::endl;
+		flag = true;
+	}
+	std::cout << "\t\t" << size;
+}
 
+int main(int argc, char* agrv[]) {
 	setlocale(LC_ALL, "rus");
 
-	int size_1{ 1000 };
-	int size_2{ 10000 };
-	int size_3{ 100000 };
-	int size_4{ 1000000 };
+	try {
 
-	auto id2 = std::thread::hardware_concurrency();
-	std::cout << "Количество аппаратных ядер - " << '\t' << id2 << std::endl;
+		std::vector<int> sizes;
 
-	std::cout << "\t\t" << size_1 << "\t\t" << size_2 << "\t\t" << size_3 << "\t\t" << size_4 << std::endl;
+		sizes.push_back(1000);
+		sizes.push_back(10000);
+		sizes.push_back(100000);
+		sizes.push_back(1000000);
+		sizes.push_back(10000000);
 
-	print_result(size_1, size_2, size_3, size_4, 1);
-	print_result(size_1, size_2, size_3, size_4, 2);
-	print_result(size_1, size_2, size_3, size_4, 4);
-	print_result(size_1, size_2, size_3, size_4, 8);
-	print_result(size_1, size_2, size_3, size_4, 16);
+		if (sizes.empty()) {
+			throw std::runtime_error("sizes is empty");
+		}
 
+		for (const auto i : sizes) {
+			print_header(i);
+		}
+
+		std::cout << std::endl;
+
+		print_result(sizes, stream_number::ONE);
+		print_result(sizes, stream_number::TWO);
+		print_result(sizes, stream_number::FOUR);
+		print_result(sizes, stream_number::EIGHT);
+		print_result(sizes, stream_number::SIXTEEN);
+	}
+
+	catch (const std::exception& e) {
+		std::cerr << "Error!" << e.what() << std::endl;
+	}
 
 	return 0;
 }
