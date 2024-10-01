ef467f61f923eb782518320591027fc8dc14bd83
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-10-01 21:07:54 +0400

    Created question

--- lesson_4_refactoring/task_1_shape/Shape.h
+++ lesson_4_refactoring/task_1_shape/Shape.h
@@ -1,20 +1,8 @@
 #pragma once
 
-enum class TypeShape {
-	LINE,
-	RECTANGLE,
-	PARALLELEPIPED,
-	CIRCLE,
-	CYLINDER,
-	UNDEFINED
- };
 
 class Shape
 {
 public:
 	Shape();
-	TypeShape getType() { return type; }
-
-protected:
-	TypeShape type = TypeShape::UNDEFINED;
 };
